487edf59a3de8b5095e5140d64f2b5c9a705969f
arnaudmg <arno.mege@gmail.com>
2020-04-05 22:46:12 +0200

    Fixing setTimeout - Button clickable

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 const start = document.querySelector('.start')
 const cubic = document.querySelector('.dice')
 let counter = 0
+var hasKeyframePlayed = false
 
+setTimeout(function() { hasKeyframePlayed = true; }, 5000);
 
-function random (){
+function random() {
     const nombre = Math.floor((Math.random() * 6) + 1);
-    if (nombre == cubic.getAttribute('data-face')){
+    if (nombre == cubic.getAttribute('data-face')) {
         return random()
-    }
-    else{
+    } else {
         return nombre
     }
 }
 
-
-
-start.addEventListener('click', () =>{
+start.addEventListener('click', () => {
+    if(!hasKeyframePlayed) return
     const number = random()
     cubic.setAttribute('data-face', number)
-    var table = document.querySelector(".averageTable");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    counter ++
+    const table = document.querySelector(".averageTable");
+    const row = table.insertRow(-1);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    counter++
     cell1.innerHTML = `You have roll the dice ${counter} times`;
-    cell2.innerHTML = number ;
-}
-)
-console.log(cubic);
-
+    cell2.innerHTML = number;
+})
 
