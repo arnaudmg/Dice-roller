1479d1d5c1730c32af09489cecc47c1f362eda1b
arnaudmg <arno.mege@gmail.com>
2020-04-05 21:44:20 +0200

    Introducing Table for average number

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const start = document.querySelector('.start')
 const cubic = document.querySelector('.dice')
+let counter = 0
 
 
 function random (){
@@ -12,8 +13,18 @@ function random (){
     }
 }
 
+
+
 start.addEventListener('click', () =>{
-    cubic.setAttribute('data-face', random())
+    const number = random()
+    cubic.setAttribute('data-face', number)
+    var table = document.querySelector(".averageTable");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    counter ++
+    cell1.innerHTML = `You have roll the dice ${counter} times`;
+    cell2.innerHTML = number ;
 }
 )
 console.log(cubic);
