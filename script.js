e1dec76637ff73392421aa81d59c1c98f4628ec1
arnaudmg <arno.mege@gmail.com>
2020-04-16 02:04:23 +0200

    Update - Fixing bug - Adding bg

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
 const start = document.querySelector('.start')
 const cubic = document.querySelector('.dice')
 let counter = 0
-let value = document.querySelector('.value')
+const value = document.querySelector('.value')
 let hasKeyframePlayed = false
-let length = document.querySelector(".averageTable").rows.length;
+let length = document.querySelector('.averageTable').rows.length;
 const table = document.querySelector('.averageTable')
+const subBackground = document.querySelectorAll('.subBackground img')
 let sumValue = 0
 
 setTimeout(function () {
-    hasKeyframePlayed = true;
-}, 5000);
+    hasKeyframePlayed = true
+    start.classList.remove('noclick')
+}, 5000)
 
 function random() {
-    const nombre = Math.floor((Math.random() * 6) + 1);
-    if (nombre == cubic.getAttribute('data-face')) {
+    const number = Math.floor((Math.random() * 6) + 1);
+    if (number == cubic.getAttribute('data-face')) {
         return random()
     } else {
-        return nombre
+        return number
     }
 }
 
 start.addEventListener('click', () => {
-    if (!hasKeyframePlayed) return
+    if (!hasKeyframePlayed) return 
     const number = random()
     cubic.setAttribute('data-face', number)
-    const table = document.querySelector(".averageTable tbody")
+    const table = document.querySelector('.averageTable tbody')
     const row = table.insertRow(-1)
     const cell1 = row.insertCell(0)
     const cell2 = row.insertCell(1)
     counter++
-    cell1.innerHTML = `You have roll the dice ${counter} times`
-    cell2.innerHTML = number
-    
-    
-    const audio = document.querySelector('#audio')
+    cell1.textContent = `You have roll the dice ${counter} times`
+    cell2.textContent = number
+
+    const audio = document.querySelector('.audio')
     audio.play()
 
     sumValue += number
-    let average = sumValue / (table.rows.length - 1)
+    const average = sumValue / (table.rows.length - 1)
 
-    value.innerText = average
-    
+    value.innerText = average.toFixed(3)
+})
+
+document.querySelectorAll('.background-selector').forEach((bgselector) => {
+    bgselector.addEventListener("click", () => {
+
+        document.body.style.backgroundImage = `url(${bgselector.getAttribute('data-path')})`
+    })
 })
 
 //     for(var i = 1; i < table.rows.length; i++){
